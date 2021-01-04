096c40d79bb0783b7f95afa53dc1e8c7074c7b38
RookieTalent <2544472820@qq.com>
2021-01-04 22:41:32 +0800

    增加了题目

--- src/test/java/run/zykj/app/AuthdemoApplicationTests.java
+++ src/test/java/run/zykj/app/AuthdemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
@@ -18,12 +19,25 @@ class AuthdemoApplicationTests {
     @Value("${spring.datasource.password}")
     private String password;
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Autowired
     private StringEncryptor demoStringEncryptor;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     void test1(){
         String encrypt = demoStringEncryptor.encrypt(password);
         System.out.println("encrypt = " + encrypt);
+    }
+
+    @Test
+    void test2(){
+        String obj = (String) redisTemplate.opsForValue().get("sys_config:sys.index.skinName");
+
+        System.out.println("obj.length() = " + obj.length());
     }
 }
